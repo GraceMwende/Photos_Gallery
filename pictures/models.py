0d7254ca7b5de1f85744b527fedddc165b3f5054
GraceMwende <grace.micheni@student.moringaschool.com>
2022-02-28 18:24:50 +0300

    add models

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+  cat_name = models.CharField(max_length=30)
+
+  def __str__(self):
+    return self.cat_name
+
+class Location(models.Model):
+  loc_name = models.CharField(max_length=30)
+
+  def __str__(self):
+    return self.loc_name
+
+class Image(models.Model):
+  image = models.ImageField(upload_to='images/',null=False)
+  img_name = models.CharField(max_length=30)
+  img_description = models.CharField(max_length=30)
+  location = models.ForeignKey(Location, on_delete=models.CASCADE)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.img_name
+
--- pictures/urls.py
+++ pictures/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path,re_path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
   path('',views.welcome, name='welcome'),
   path('today/',views.test, name='testing'),
-  path('kesho/',views.amazing, name='testing2'),
-  re_path('archives/(\d{4}-\d{2})/',views.wuueh, name='testing')
+  path('kesho/',views.amazing, name='testing2')
 ]
+
+if settings.DEBUG:
+  urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
